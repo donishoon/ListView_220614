aea1f76e711978822b6d1d285999e906f90e8c88
강도훈 <fghfgh3255@gmail.com>
2022-06-14 15:19:53 +0900

    학생 데이터를 xml과 결합해서 리스트뷰에 뿌려주는, adapter 클래스 제작 중

---
+++ app/src/main/java/com/dohn/listview_220614/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.dohn.listview_220614.adapters
+
+import android.widget.ArrayAdapter
+import com.dohn.listview_220614.datas.StudentData
+
+class StudentAdapter : ArrayAdapter<StudentData>() {
+}
