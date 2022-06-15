0ed31c0c5ffe4826dc9e869926dc0a8dca05248b
강도훈 <fghfgh3255@gmail.com>
2022-06-15 11:40:15 +0900

    어댑터 뼈대작업 마무리

--- app/src/main/java/com/dohn/listview_220614/adapters/StudentAdapter.kt
+++ app/src/main/java/com/dohn/listview_220614/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.dohn.listview_220614.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.dohn.listview_220614.R
 import com.dohn.listview_220614.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,21 @@ class StudentAdapter(
     val resId: Int,
     val mList: ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if ( tempRow == null ) {
+            tempRow = LayoutInflater.from( mContext ).inflate( R.layout.student_list_item, null )
+        }
+
+        val row = tempRow!!
+
+        return row
+
+
+    }
+
+
 }
