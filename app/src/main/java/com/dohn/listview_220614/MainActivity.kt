e40f7383c2ceb6014cd16e80ce5c0f6a63cd4195
강도훈 <fghfgh3255@gmail.com>
2022-06-15 12:29:26 +0900

    리스트뷰의 아이템 클릭 이벤트 처리 (토스트 띄우기)

--- app/src/main/java/com/dohn/listview_220614/MainActivity.kt
+++ app/src/main/java/com/dohn/listview_220614/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dohn.listview_220614
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.dohn.listview_220614.adapters.StudentAdapter
 import com.dohn.listview_220614.datas.StudentData
 import kotlinx.android.synthetic.main.activity_main.*
@@ -29,6 +30,15 @@ class MainActivity : AppCompatActivity() {
         mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList )
 
         studentListView.adapter = mAdapter
+
+        studentListView.setOnItemClickListener { parent, view, position, id ->
+            
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${clickedStudent.name} 클릭됨", Toast.LENGTH_SHORT).show()
+
+
+        }
 
     }
 }
